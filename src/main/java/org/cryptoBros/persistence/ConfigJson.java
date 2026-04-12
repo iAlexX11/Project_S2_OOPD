0dc409741115302c379501904718628b3b43ccb8
guijanson007 <guilhermejanson@gmail.com>
2026-04-12 20:38:29 +0200

    Initial logic for admin password

---
+++ src/main/java/org/cryptoBros/persistence/ConfigJson.java
@@ -0,0 +1,37 @@
+package org.cryptoBros.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.cryptoBros.persistence.Deserializers.ConfigDeserializer;
+import org.cryptoBros.persistence.Exceptions.ConfigFileNotFoundException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigJson implements ConfigPersistence {
+    private final Gson gson;
+    private static final String CONFIG_FILEPATH = "config.json";
+
+    public ConfigJson() {
+        gson = new GsonBuilder().registerTypeAdapter(Config.class, new ConfigDeserializer()).create();
+    }
+
+    private Config readConfig() throws ConfigFileNotFoundException {
+        try (FileReader reader = new FileReader(CONFIG_FILEPATH)) {
+            return gson.fromJson(reader, Config.class);
+        } catch (IOException e) {
+            throw new ConfigFileNotFoundException("Failed to read config file");
+        }
+    }
+
+    @Override
+    public String readAdminPassword() throws ConfigFileNotFoundException {
+        return readConfig().adminPassword();
+    }
+
+    @Override
+    public DbCredentials readCredentials() throws ConfigFileNotFoundException {
+        Config c = readConfig();
+        return new DbCredentials(c.port(), c.ip(), c.dbName(), c.username(), c.password());
+    }
+}
