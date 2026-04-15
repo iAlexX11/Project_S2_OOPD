b8fd0d19d0e2b616af4dc2ada4a4e32b67468941
iAlexX11 <alejandrogarciacuellar1@gmail.com>
2026-04-15 13:24:51 +0100

    Codes merged

---
+++ src/main/java/org/cryptoBros/business/AccountManager.java
@@ -0,0 +1,66 @@
+package org.cryptoBros.business;
+import org.passay.*;
+import org.mindrot.jbcrypt.BCrypt;
+import static org.passay.EnglishCharacterData.*;
+
+public class AccountManager {
+
+	public boolean checkPassword(char[] password) {
+
+		PasswordValidator validator = new PasswordValidator(
+				new LengthRule(6, 20),
+				new CharacterRule(UpperCase, 1),
+				new CharacterRule(LowerCase, 1),
+				new CharacterRule(Digit, 1),
+				new CharacterRule(Special, 1),
+				new WhitespaceRule()
+		);
+
+		PasswordData data = new PasswordData(new String(password));
+		RuleResult result = validator.validate(data);
+		data = null;
+
+		return result.isValid();
+	}
+
+	public String hashPassword(char[] password) {
+		return BCrypt.hashpw(new String(password), BCrypt.gensalt(12));
+	}
+
+	public boolean checkEmail(String email) {
+		boolean hasDot = false;
+		boolean hasName = false; //
+		boolean hasAT = false; //
+		boolean hasDomain = false;
+		boolean hasMail = false; // gmail, yahoo...
+		int nDots = 0;
+		if (!email.isEmpty()) {
+			for (int i = 0; i < email.length(); i++) {
+				if (email.charAt(i) == '@') {
+					hasAT = true;
+					i++;
+					for (int j = i; j < email.length(); j++) {
+						if (email.charAt(j) == '.') {
+							hasDot = true;
+							nDots++;
+						}
+						if (Character.isLetter(email.charAt(j)) && !hasDot) {
+							hasMail = true;
+						}
+						if (Character.isLetter(email.charAt(j)) && hasDot) {
+							hasDomain = true;
+						}
+					}
+					break;
+				}
+				if (Character.isLetterOrDigit(email.charAt(i)) && !hasAT) {
+					hasName = true;
+				}
+			}
+			if (hasName && hasAT && hasDomain && hasMail && nDots == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- src/main/java/org/cryptoBros/presentation/Controllers/RegistrationController.java
+++ src/main/java/org/cryptoBros/presentation/Controllers/RegistrationController.java
@@ -1,43 +1,72 @@
 package org.cryptoBros.presentation.Controllers;
 
-import org.cryptoBros.presentation.Views.ButtonEnumeration;
-import org.cryptoBros.presentation.Views.LoginView;
-import org.cryptoBros.presentation.Views.MainFrame;
-import org.cryptoBros.presentation.Views.SignUpView;
+import org.cryptoBros.business.AccountManager;
+import org.cryptoBros.presentation.Views.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class RegistrationController implements ActionListener {
 
-    private FrameController frameController;
-    private LoginView loginView;
-    private SignUpView signUpView;
+	private final FrameController frameController;
+	private final LoginView loginView;
+	private final SignUpView signUpView;
+	private final AccountManager accountManager;
 
-    public RegistrationController (FrameController frameController) {
-        this.loginView = new LoginView();
-        this.signUpView = new SignUpView();
-        this.frameController = frameController;
-        loginView.setActions(this);
-        signUpView.setActions(this);
-    }
+	public RegistrationController (FrameController frameController) {
+		this.loginView = new LoginView();
+		this.signUpView = new SignUpView();
+		this.frameController = frameController;
+		this.accountManager = new AccountManager();
+		loginView.setActions(this);
+		signUpView.setActions(this);
+	}
 
-    public void login() {
-        frameController.displayContent(loginView);
-    }
+	public void login() {
+		frameController.displayContent(loginView);
+	}
 
-    public void signUp() {
-        frameController.displayContent(signUpView);
-    }
+	public void signUp() {
+		frameController.displayContent(signUpView);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        ButtonEnumeration buttonEnumeration = ButtonEnumeration.valueOf(e.getActionCommand());
-        switch (buttonEnumeration) {
-            case CONFIRM_SIGNUP -> System.out.println("confirm signUp");
-            case LOGIN -> login();
-            case CONFIRM_LOGIN ->  System.out.println("confirm login");
-            case SIGNUP -> signUp();
-        }
-    }
+	private boolean signUpCredentialsFormat () {
+		boolean emailOk = accountManager.checkEmail(signUpView.getEmail());
+		boolean passwordOk = accountManager.checkPassword(signUpView.getPassword());
+		boolean difPassword = Arrays.equals(signUpView.getPassword(), signUpView.getConfirmPassword());
+
+		if (emailOk && passwordOk && difPassword) {
+			return true;
+		} else if (!emailOk && !passwordOk) {
+			ErrorsView.showError("Wrong email and password format");
+		} else if (!emailOk) {
+			ErrorsView.showError("Wrong email format");
+		} else if (!passwordOk) {
+			ErrorsView.showError("Wrong password format");
+		} else {
+			ErrorsView.showError("Password must match");
+		}
+		return false;
+	}
+
+	public void registerNewUser() {
+		if (signUpCredentialsFormat()) {
+			String password = accountManager.hashPassword(signUpView.getPassword());
+			String email = signUpView.getEmail();
+			String userName = signUpView.getName();
+
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		ButtonEnumeration buttonEnumeration = ButtonEnumeration.valueOf(e.getActionCommand());
+		switch (buttonEnumeration) {
+			case CONFIRM_SIGNUP -> registerNewUser();
+			case LOGIN -> login();
+			case CONFIRM_LOGIN -> System.out.println("User logged correctly");
+			case SIGNUP -> signUp();
+		}
+	}
 }
