2ae1ae00b48aeeb347ef8d90d473fc6858a006a6
guijanson007 <guilhermejanson@gmail.com>
2026-04-15 12:50:32 +0200

    crypto start

---
+++ src/main/java/org/cryptoBros/persistence/SQL/CryptoSQL.java
@@ -0,0 +1,39 @@
+package org.cryptoBros.persistence.SQL;
+
+import org.cryptoBros.business.Crypto;
+import org.cryptoBros.persistence.CryptoPersistence;
+
+import java.util.List;
+
+public class CryptoSQL implements CryptoPersistence {
+
+    @Override
+    public Crypto getCrypto(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Crypto> getAllCrypto() {
+        return List.of();
+    }
+
+    @Override
+    public void addCrypto(Crypto newCrypto) {
+
+    }
+
+    @Override
+    public void removeCrypto(String name) {
+
+    }
+
+    @Override
+    public void updateCrypto(String name, Crypto newCrypto) {
+
+    }
+
+    @Override
+    public void updatePrice(String name, double newPrice) {
+
+    }
+}
