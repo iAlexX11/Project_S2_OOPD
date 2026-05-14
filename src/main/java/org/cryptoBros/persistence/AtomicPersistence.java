d66e828e795697eb44642bbc0c8a8ca57f2e4cc8
guijanson007 <guilhermejanson@gmail.com>
2026-05-14 18:14:28 +0200

    Delete unused function

--- src/main/java/org/cryptoBros/persistence/AtomicPersistence.java
+++ src/main/java/org/cryptoBros/persistence/AtomicPersistence.java
@@ -14,6 +14,4 @@ public interface AtomicPersistence {
     void deleteCryptoWithBot(String symbol)
             throws DbConnectionException, CryptoNotFoundException;
 
-    void purchaseCryptoWithBalanceUpdate(long userId, String symbol, double currentPrice, double units, double totalCost)
-            throws DbConnectionException, CryptoNotFoundException;
 }
